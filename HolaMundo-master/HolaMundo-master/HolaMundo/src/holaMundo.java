adbbbbb3c185745466357e4e1a8c2986cc2c0355
Grumg <grumg97@gmail.com>
2021-11-24 12:03:37 +0100

    Update holaMundo.java
    Cambio en el saludo

--- HolaMundo-master/HolaMundo-master/HolaMundo/src/holaMundo.java
+++ HolaMundo-master/HolaMundo-master/HolaMundo/src/holaMundo.java
@@ -2,6 +2,7 @@
 public class holaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
-		System.out.println("Hola soy Alberto");
+		System.out.println("Hola soy Juan Fernando");
+		System.out.println("Esto es un cambio que se verá reflejado en git hub");
 	}
 }
